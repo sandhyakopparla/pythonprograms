c09177993aa043e06525de254870839fcaa8d3ce
sandhyakopparla <sandhyakopparla2413@gmail.com>
2021-07-21 14:38:19 +0530

    sum changes

--- list.py
+++ list.py
@@ -1,4 +1,5 @@
-names=["siddharth","akshay","sandhya","anu",23,45,78]
+#names=["siddharth","akshay","sandhya","anu",23,45,78]
+names=["sandhya","akshay","anu"]
 print(names[2:4])
 # del names[3]
 #  print(names)
@@ -10,4 +11,8 @@ print(len(names))
 students=["sandhya","sanju","sandy"]
 print(names+students)
 print(students*2)
+names.sort()
+print(names)
+
+
 
