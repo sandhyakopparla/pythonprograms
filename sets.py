d8d2ad7c0f807b4d44be05f5d9abcfb10ec3cf85
sandhyakopparla <sandhyakopparla2413@gmail.com>
2021-07-21 14:27:27 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,12 @@
+fruits={"orange","grapes","apple","orange"}
+juice={"pineapple","orange"}
+# fruits.add("banana")
+# fruits.remove("orange")
+# print(fruits)
+print(fruits.intersection(juice))
+print(fruits.union(juice))
+print(fruits.issubset(juice))
+print(fruits.issuperset(juice))
+print(fruits.difference(juice))
+
+
