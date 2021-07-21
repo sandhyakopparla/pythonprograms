253775df8696204a796d26664e3271d9872ac205
sandhyakopparla <sandhyakopparla2413@gmail.com>
2021-07-21 12:10:21 +0530

    list membership

---
+++ dic.py
@@ -0,0 +1,7 @@
+students={"name":"siddhu","age":22,"company":"prodapt"}
+students["age"]=23
+del students["age"]
+print(students)
+students.clear()
+print(students.keys())
+print(students.values())
---
+++ tuple.py
@@ -0,0 +1,18 @@
+names=("sandhya","siddharth","aadrika")
+integer=(45,67)
+#names[0]="anu"
+#del names[0]
+print(names)
+print(len(names))
+print(names[0])
+print(names[0:2])
+#students=(67)
+print(names+integer)
+name="sandhya"
+if(name in names):
+    print("it is present")
+else:
+    print("it is not present")
+print(max(integer))
+print(min(integer))
+print(list(names))
