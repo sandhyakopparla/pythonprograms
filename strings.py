136a71dfad4b4e668a07d259af3398dbeb710dc6
sandhyakopparla <sandhyakopparla2413@gmail.com>
2021-07-23 05:53:07 +0530

    palindrome

---
+++ strings.py
@@ -0,0 +1,43 @@
+data=input("enter the string:")
+list=[7,8,7,6]
+string="hii sandhya"
+print(string)
+x=slice(3)
+print(data[0:6])
+print(data[x])
+print(data.capitalize())
+print(data.title())
+print(data.upper())
+print(data.lower())
+print(data.strip())#it gives the clean version of the string
+print(data.replace("hloo","hii"))
+print(list.count(7))
+print(data.casefold())
+print(data.encode())
+print(data.find("hloo"))#it shows at what index value the hloo is started
+print(data.replace("hello","hi"))
+print(data[:10])#from starting to upto 10th index it will print the output
+print(data[:])#it will print all the data
+print(data[-3:-1])#here we have to give a value from left to right
+print(data.index("l"))
+print(data.isalnum())
+print(data.isalpha())
+print(data.isdigit())
+print(data.split("hloo"))#it will split the word
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
